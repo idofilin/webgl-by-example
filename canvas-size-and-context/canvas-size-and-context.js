3602ec8da983503ab26ee5f2b07b88eed0990a6e
Ido Filin <ifilin@iki.fi>
2015-11-26 22:41:29 +0200

    text reformatting

--- canvas-size-and-context/canvas-size-and-context.js
+++ canvas-size-and-context/canvas-size-and-context.js
@@ -13,9 +13,11 @@ window.addEventListener("load", function() {
         + "Your browser or device may not support WebGL.";
       return;
     } 
-    paragraph.innerHTML = "Congratulations! Your browser supports WebGL. "
+    paragraph.innerHTML = 
+      "Congratulations! Your browser supports WebGL. "
       + "Now, compare the two canvases.";
-    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.viewport(0, 0, 
+      gl.drawingBufferWidth, gl.drawingBufferHeight);
     gl.enable(gl.SCISSOR_TEST);
     gl.scissor(30, 10, 60, 60);
     gl.clearColor(1.0, 1.0, 0.0, 1.0);
